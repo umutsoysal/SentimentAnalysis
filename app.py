8206602039e6d06375e70d01c52c1c0a6ae2fe9d
Umut Soysal <us.soysal@gmail.com>
2021-11-16 00:13:55 -0600

    method reorder

--- app.py
+++ app.py
@@ -7,8 +7,26 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import tensorflow as tf
 import re
 
+def preProcess_data(text):
+    text = text.lower()
+    new_text = re.sub('[^a-zA-z0-9\s]','',text)
+    new_text = re.sub('rt', '', new_text)
+    return new_text
+
 
 app = FastAPI()
+
+data = pd.read_csv('archive/Sentiment.csv')
+tokenizer = Tokenizer(num_words=2000, split=' ')
+tokenizer.fit_on_texts(data['text'].values)
+
+def my_pipeline(text):
+    text_new = preProcess_data(text)
+    X = tokenizer.texts_to_sequences(pd.Series(text_new).values)
+    X = pad_sequences(X, maxlen=28)
+    return X
+
+
 
 @app.get('/predict', response_class=HTMLResponse)
 def take_inp():
@@ -17,23 +35,6 @@ def take_inp():
         <input maxlength="28" name="text" type="text" value="Text Emotion to be tested" />
         <input type="submit" />'''
 
-
-
-data = pd.read_csv('archive/Sentiment.csv')
-tokenizer = Tokenizer(num_words=2000, split=' ')
-tokenizer.fit_on_texts(data['text'].values)
-
-def preProcess_data(text):
-    text = text.lower()
-    new_text = re.sub('[^a-zA-z0-9\s]','',text)
-    new_text = re.sub('rt', '', new_text)
-    return new_text
-
-def my_pipeline(text):
-    text_new = preProcess_data(text)
-    X = tokenizer.texts_to_sequences(pd.Series(text_new).values)
-    X = pad_sequences(X, maxlen=28)
-    return X
 
 @app.get('/')
 def basic_view():
